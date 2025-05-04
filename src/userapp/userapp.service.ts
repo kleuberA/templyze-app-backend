a2d75dfe10e865e44d741d48d0a6f190280ac941
Kleuber Silva <kleuber20@hotmail.com>
2025-05-04 14:18:34 -0300

    feat(userapp): implementar serviço para obter informações do usuário por ID

--- src/userapp/userapp.service.ts
+++ src/userapp/userapp.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserappService {}
+export class UserappService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getUserInfoById(userID: string) {
+        const user = await this.prisma.user.findUnique({
+            where: { id: userID },
+            include: {
+                purchases: true,
+            },
+        })
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+            purchases: user.purchases
+        };
+
+    }
+
+}

db84e654b65a08f2e357499fb5dca10661db2fed
Kleuber Silva <kleuber20@hotmail.com>
2025-05-05 15:45:05 -0300

    feat(userapp): implementar controlador e método para obter informações do usuário por ID

--- src/userapp/userapp.controller.ts
+++ src/userapp/userapp.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import { UserappService } from './userapp.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+@ApiTags('User App')
 @Controller('userapp')
-export class UserappController {}
+export class UserappController {
+    constructor(private readonly userAppService: UserappService) { }
+
+
+    @ApiBearerAuth()
+    @Get('user/:id')
+    async getUserInfoById(@Param('id') userID: string, @Res() resp: Response) {
+        try {
+            await this.userAppService.getUserInfoById(userID);
+            return resp.status(HttpStatus.OK).json({ message: "Get info user app!" });
+        } catch (error) {
+            return resp.status(HttpStatus.BAD_REQUEST).json({ message: "Failed to get user app!", error: error.message });
+        }
+    }
+
+}

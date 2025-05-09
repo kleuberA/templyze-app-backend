79446f461f03e6ef107d9ae50bb7e0a5ea95630c
Kleuber Silva <kleuber20@hotmail.com>
2025-05-09 14:53:23 -0300

    feat(userapp): adicionar método para atualizar informações do usuário por ID

--- src/userapp/userapp.controller.ts
+++ src/userapp/userapp.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Delete, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Res } from '@nestjs/common';
 import { UserappService } from './userapp.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
+import { UserAppInfoByIdEntity } from './entity/userAppInfoById.entity';
 @ApiTags('User App')
 @Controller('userapp')
 export class UserappController {
     constructor(private readonly userAppService: UserappService) { }
-
 
     @ApiBearerAuth()
     @Get('user-app/:id')
@@ -16,6 +16,18 @@ export class UserappController {
             return resp.status(HttpStatus.OK).json({ message: "Get info user app!" });
         } catch (error) {
             return resp.status(HttpStatus.BAD_REQUEST).json({ message: "Failed to get user app!", error: error.message });
+        }
+    }
+
+    @ApiBearerAuth()
+    @ApiBody({ type: UserAppInfoByIdEntity })
+    @Patch('user-app/update/:id')
+    async updateUserInfoById(@Param('id') userID: string, @Res() resp: Response, @Body() updateUserApp: UserAppInfoByIdEntity) {
+        try {
+            await this.userAppService.updateUserAppInfoById(userID, updateUserApp);
+            return resp.status(HttpStatus.OK).json({ message: "Update user app!" });
+        } catch (error) {
+            return resp.status(HttpStatus.BAD_REQUEST).json({ message: "Failed to update user app!", error: error.message });
         }
     }
 

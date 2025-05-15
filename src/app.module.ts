a762a6974efaab21e0db394ce4a05e535b154d90
Kleuber Silva <kleuber20@hotmail.com>
2025-05-15 17:49:45 -0300

    feat(task): adicionar módulo, controlador e serviço para gerenciamento de tarefas

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { AppLibraryModule } from './app-library/app-library.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { UserappModule } from './userapp/userapp.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule, AppLibraryModule, UserappModule],
+  imports: [AuthModule, PrismaModule, UserModule, AppLibraryModule, UserappModule, TaskModule],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_GUARD,

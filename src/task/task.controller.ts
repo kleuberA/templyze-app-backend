b241c1602d019a1cca906577767f7432a9b0e8f8
Kleuber Silva <kleuber20@hotmail.com>
2025-05-17 20:11:02 -0300

    feat(task): implementar controlador para obter tarefas por ID de usuário

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 
+@ApiTags('Task')
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+    constructor(private readonly taskService: TaskService) { }
+
+    @ApiBearerAuth()
+    @Get('/all-tasks-by-id/:userid')
+    async getAllTasksByUserId(@Param('id') userID: string, @Res() resp: Response) {
+        try {
+            const tasks = await this.taskService.getAllTasksByUserId(userID);
+            return resp.status(200).json({ message: "Tasks fetched successfully!", tasks });
+        } catch (error) {
+            return resp.status(400).json({ message: "Failed to fetch tasks!", error: error.message });
+        }
+    }
+
+}

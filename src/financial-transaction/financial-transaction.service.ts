d9fa7f65807c98d6fda3b76d477514db4ee85225
Kleuber Silva <kleuber20@hotmail.com>
2025-06-02 13:55:50 -0300

    feat(financial-transaction): adicionar métodos para obter transações financeiras por ID de usuário e por ID de transação

--- src/financial-transaction/financial-transaction.service.ts
+++ src/financial-transaction/financial-transaction.service.ts
@@ -1,15 +1,36 @@
-import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class FinancialTransactionService {
     constructor(private readonly prisma: PrismaService) { }
 
     async getFinancialTransactionsByUserId(userId: string) {
+
+        const user = await this.prisma.user.findUnique({
+            where: { id: userId },
+        });
+
+        if (!user) {
+            throw new Error('User not found!');
+        }
+
         return this.prisma.financialTransaction.findMany({
             where: { userId },
             orderBy: { createdAt: 'desc' },
         });
+    }
+
+    async getFinancialTransactionById(id: string) {
+        const transaction = await this.prisma.financialTransaction.findUnique({
+            where: { id },
+        });
+
+        if (!transaction) {
+            throw new Error('Financial transaction not found!');
+        }
+
+        return transaction;
     }
 
 }
